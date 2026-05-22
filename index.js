141bf3444fa9ce485b72988b8079032ff8102286
Летягин Алексей <zencorzglobalis@gmail.com>
2026-05-22 17:39:44 +0300

    323

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use('/api/', limiter);
 app.use('/api/admin/login', loginLimiter);
 
 app.use(cors({
-    origin: true,
+    origin: 'localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY'],
