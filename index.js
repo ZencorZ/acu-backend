4eb7fc44ff51447417182e5e350979d63258e2cc
Летягин Алексей <zencorzglobalis@gmail.com>
2026-05-22 20:20:23 +0300

    6765

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 const PORT = process.env.PORT || 3001;
-const HOST = '92.51.39.109';
+const HOST = process.env.HOST || '0.0.0.0';
 
 // ========== НАСТРОЙКИ БЕЗОПАСНОСТИ ==========
 
